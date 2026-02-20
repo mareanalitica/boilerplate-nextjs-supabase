8eb496c0e4c6cf8dbe378695c2e10bb8f3be512b
mareanalitica <mareanalitica@gmail.com>
2026-02-20 12:30:33 -0300

    docs: inicial pages

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,8 +9,8 @@ const defaultUrl = process.env.VERCEL_URL
 
 export const metadata: Metadata = {
   metadataBase: new URL(defaultUrl),
-  title: "Next.js and Supabase Starter Kit",
-  description: "The fastest way to build apps with Next.js and Supabase",
+  title: "Boilerplate v1",
+  description: "A maneira mais simples de iniciar seu negócio",
 };
 
 const geistSans = Geist({
